4496f652df845da1d60a9e5ceb4c3635bf4a8bf2
Patrick Tone <pt@patrickt.one>
2019-01-22 07:30:32 -0600

    Finished the identicon project!

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,10 +1,13 @@
 defmodule Identicon do
   def main(input) do
     input
-    |> hash_input()
-    |> pick_color()
-    |> build_grid()
-    |> filter_odd_squares()
+    |> hash_input
+    |> pick_color
+    |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
   end
 
   @doc """
@@ -21,8 +24,9 @@ defmodule Identicon do
 
   """
   def hash_input(input) do
-    hex = :crypto.hash(:md5, input)
-    |> :binary.bin_to_list()
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list
 
     %Identicon.Image{hex: hex}
   end
@@ -61,8 +65,8 @@ defmodule Identicon do
       hex_list
       |> Enum.chunk_every(3, 3, :discard)
       |> Enum.map(&mirror_row/1)
-      |> List.flatten()
-      |> Enum.with_index()
+      |> List.flatten
+      |> Enum.with_index
 
     %Identicon.Image{image | grid: grid}
   end
@@ -92,9 +96,60 @@ defmodule Identicon do
   """
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
     filtered_grid =
-      grid
-      |> Enum.filter(fn {number, _} -> rem(number, 2) == 0 end)
+      Enum.filter grid, fn {number, _} ->
+        rem(number, 2) == 0
+      end
 
     %Identicon.Image{image | grid: filtered_grid}
+  end
+
+  @doc """
+    Generate the top-left and bottom-right coordinates for each index in the grid
+
+  ## Examples
+
+      iex> image = %Identicon.Image{grid: [{2, 0}, {4, 1}, {6, 5}]}
+      iex> Identicon.build_pixel_map(image)
+      %Identicon.Image{
+        color: nil,
+        grid: [{2, 0}, {4, 1}, {6, 5}],
+        hex: nil,
+        pixel_map: [{{0, 0}, {60, 60}}, {{60, 0}, {120, 60}}, {{0, 60}, {60, 120}}]
+      }
+  """
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map =
+      Enum.map grid, fn {_, index} ->
+        horizontal = rem(index, 5) * 60
+        vertical = div(index, 5) * 60
+
+        top_left = {horizontal, vertical}
+        bottom_right = {horizontal + 60, vertical + 60}
+
+        {top_left, bottom_right}
+      end
+
+    %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  @doc """
+    Generate the image
+  """
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(300, 300)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn {start, stop} ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
+  end
+
+  @doc """
+    Save the image to a file
+  """
+  def save_image(image, input) do
+    File.write("#{input}.png", image)
   end
 end
